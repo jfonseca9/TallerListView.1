5686afe1abdde49052042d3b60148db7b34a44f1
Jaime Fonseca <j.efonsecaguerrero@gmail.com>
2018-04-15 18:51:40 -0500

    TallerListView

---
+++ app/src/main/java/com/example/estan/calculadorageometrica/Opciones.java
@@ -0,0 +1,13 @@
+package com.example.estan.calculadorageometrica;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class Opciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opciones);
+    }
+}
